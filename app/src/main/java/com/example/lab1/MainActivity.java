05d4c80b643d60ab1c40d11264d5f0940d857727
ddudkovskaya <ddudkovskaya@mail.ru>
2022-03-10 12:23:04 +0300

    second

--- app/src/main/java/com/example/lab1/MainActivity.java
+++ app/src/main/java/com/example/lab1/MainActivity.java
@@ -4,6 +4,9 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.TextView;
 
+import org.apache.commons.codec.DecoderException;
+import org.apache.commons.codec.binary.Hex;
+
 import com.example.lab1.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
@@ -23,9 +26,28 @@ public class MainActivity extends AppCompatActivity {
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
+        int res = initRng();
+        byte[] v=randomBytes(10);
+
         // Example of a call to a native method
-        TextView tv = binding.sampleText;
+       // TextView tv = binding.sampleText;
+        TextView tv = findViewById(R.id.sample_text);
         tv.setText(stringFromJNI());
+    }
+
+    public static byte[] stringToHex(String s)
+    {
+
+        byte[] hex;
+        try
+        {
+            hex = Hex.decodeHex(s.toCharArray());
+        }
+        catch (DecoderException ex)
+        {
+            hex = null;
+        }
+        return hex;
     }
 
     /**
@@ -33,4 +55,8 @@ public class MainActivity extends AppCompatActivity {
      * which is packaged with this application.
      */
     public native String stringFromJNI();
+    public static native int initRng();
+    public static native byte[] randomBytes(int no);
+    public static native byte[] encrypt(byte[] key, byte[] data);
+    public static native byte[] decrypt(byte[] key, byte[] data);
 }
